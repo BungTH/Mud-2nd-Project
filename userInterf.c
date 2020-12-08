8127c100c051452093d96545c49666e1dfba7af2
BungkapTH <www.bung555@gmail.com>
2020-12-08 15:52:12 +0700

    Version 1.0.0 (stable)
    Initial Commit
    Changes :
    -Creating C files
    -UI polishment
    
    signed
    Teerasan Rattanaruengkul

---
+++ userInterf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;             /*recieve choice data*/
+    char inp[64];           /*recieve data from users*/
+
+    printf("        Welcome to CPE student Database Manager        \n\n\n");
+    printf("*******************************************************\n\n");
+    printf("\tHere are available choices : \n");
+    printf("\t\t1. Display all records\n");
+    printf("\t\t2. Add a new record\n");
+    printf("\t\t3. Search and Modify existed record\n");
+    printf("\t\t4. Exit Database Manager\n\n");
+    printf("*******************************************************\n\n");
+    printf("Choose the choice to continue : ");
+    fgets(inp,sizeof(inp),stdin);
+    sscanf(inp,"%d",&choice);
+
+    while (choice != 4)
+    {   
+        switch (choice)
+        {
+            case 1:
+            printf("1\n\n");
+            break;
+            case 2:
+            printf("2\n\n");
+            break;
+            case 3:
+            printf("3\n\n");
+            break;
+            default:
+            printf("\t\tNot valid - illegal choice\n\n");
+        }
+        printf("*******************************************************\n\n");
+        printf("\tHere are available choices : \n");
+        printf("\t\t1. Display all records\n");
+        printf("\t\t2. Add a new record\n");
+        printf("\t\t3. Search and Modify existed record\n");
+        printf("\t\t4. Exit Database Manager\n\n");
+        printf("*******************************************************\n\n");
+        printf("Choose the choice to continue : ");
+        fgets(inp,sizeof(inp),stdin);
+        sscanf(inp,"%d",&choice);
+    }
+    printf("\n\t\tExited Manager\n");
+}
