5495060ca191fe43f6c2db987d126adf422953a5
BungkapTH <www.bung555@gmail.com>
2020-12-08 17:29:49 +0700

    Version 1.0.1 (not stable)
    Adding some infarstructure of validation and header file

---
+++ header.h
@@ -0,0 +1,3 @@
+/*
+recieve 3 input from users [title firstname lastname] and validate by using 3 function above.*/
+int ValidateName();
---
+++ valid.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+*/
+int validateDate()
+{
+
+}
+
+/*
+*/
+int validateName()
+{
+
+}
+
+/*
+*/
+int validateStID()
+{
+
+}
+
+/*
+*/
+int validateControl(int choice, char valData[128])
+{
+    int valresult;          /*validation result*/
+    char inp[128];          /*recieve data from users*/
+
+    switch (choice)
+    {
+    case 1:
+
+    break;
+    case 2:
+
+    break;
+    case 3:
+
+    break;
+    case 4:
+
+    break;
+    case 5:
+
+    break;
+    case 6:
+
+    break;
+    }
+
+    return valresult;
+}
